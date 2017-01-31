7de3b27e0c54bc8fdd7465528cac0510dac80007
Waller Goble <waller.go@gmail.com>
2017-01-30 19:20:17 -0500

    test ;

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,13 +1,13 @@
-require "spec_helper"
+# require "spec_helper"
 
-RSpec.describe Calc do
-  let(:calc) { Calc.new }
+# RSpec.describe Calc do
+#   let(:calc) { Calc.new }
   
-  describe "Methods" do
-    describe "#add" do
-      it "adds two numbers together" do
-        expect calc.add(1,1).to eq 2
-      end
-    end
-  end
-end
+#   describe "Methods" do
+#     describe "#add" do
+#       it "adds two numbers together" do
+#         expect calc.add(1,1).to eq 2
+#       end
+#     end
+#   end
+# end
